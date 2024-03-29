855d81644a0a01818a973e98732f6fa5b939238c
IunKhi <145861160+IunKhi@users.noreply.github.com>
2024-03-29 14:19:03 +0300

    Update ConsoleApplication15.cpp

--- ConsoleApplication15/ConsoleApplication15.cpp
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -2,145 +2,114 @@
 
 class cashRegister {
 private:
-	int cashOnHand;
+    int cashOnHand;
 
 public:
-	cashRegister() {
-		cashOnHand = 500;
-	}
-	cashRegister(int cash) {
-		cashOnHand = cash;
-	}
+    cashRegister() {
+        cashOnHand = 500;
+    }
+    cashRegister(int cash) {
+        cashOnHand = cash;
+    }
 
-	int getCurrentBalance() {
-		return cashOnHand;
-	}
+    int getCurrentBalance() {
+        return cashOnHand;
+    }
 
-	void acceptAmount(int amount) {
-		cashOnHand += amount;
-	}
+    void acceptAmount(int amount) {
+        cashOnHand += amount;
+    }
 };
 
 class dispenserType {
 private:
-	int numberOfItems;
-	int cost;
+    int numberOfItems;
+    int cost;
 
 public:
-	dispenserType() {
-		numberOfItems = 50;
-		cost = 50;
-	}
-	dispenserType(int items, int itemCost) {
-		numberOfItems = items;
-		cost = itemCost;
-	}
+    dispenserType() {
+        numberOfItems = 50;
+        cost = 50;
+    }
+    dispenserType(int items, int itemCost) {
+        numberOfItems = items;
+        cost = itemCost;
+    }
 
-	int getNoOfItems() {
-		return numberOfItems;
-	}
+    int getNoOfItems() {
+        return numberOfItems;
+    }
 
-	int getCost() {
-		return cost;
-	}
+    int getCost() {
+        return cost;
+    }
 
-	void makeSale() {
-		numberOfItems -= 1;
-	}
+    void makeSale() {
+        numberOfItems -= 1;
+    }
 };
 
 void showSelection() {
-	std::cout << "Выберите продукт: " << std::endl;
-	std::cout << "1. Конфеты" << std::endl;
-	std::cout << "2. Чипсы" << std::endl;
-	std::cout << "3. Жевательная резинка" << std::endl;
-	std::cout << "4. Печенье" << std::endl;
+    std::cout << "Выберите продукт: " << std::endl;
+    std::cout << "1. Конфеты" << std::endl;
+    std::cout << "2. Чипсы" << std::endl;
+    std::cout << "3. Жевательная резинка" << std::endl;
+    std::cout << "4. Печенье" << std::endl;
 }
 
-void sellProduct(dispenserType& dispenser, cashRegister& cashReg) {
-	int choice;
-	std::cout << "Введите ваш выбор: ";
-	std::cin >> choice;
-    switch (choice) {
-    case 1: {
-        std::cout << "Выбраны конфеты." << std::endl;
-        std::cout << "Стоимость: " << dispenser.getCost() << std::endl;
-        std::cout << "Введите сумму: ";
-        int amount;
-        std::cin >> amount;
-
-        if (amount >= dispenser.getCost()) {
-            dispenser.makeSale();
-            cashReg.acceptAmount(dispenser.getCost());
-            std::cout << "Продукт продан. Сдача: " << amount - dispenser.getCost() << std::endl;
+void sellProduct(dispenserType& productDisp, cashRegister& cashReg) {
+    int cost = productDisp.getCost();
+    int balance = cash.getCurrentBalance();
+    int cashIn;
+    if (productDisp.getNoOfItems() > 0) {
+        std::cout << "Стоимость товара: " << cost << "руб. ";
+        std::cin >> cashIn;
+        if (cashIn >= cost) {
+            if (balance >= cashIn - cost) {
+                productDisp.makeSale();
+                cash.acceptAmount(cost);
+                std::cout << "Ваш товар!\n";
+                std::cout << "Ваша сдача: " << cashIn - cost << "руб.\n";
+            }
+            else {
+                std::cout << "В автомате нет сдачи :(\nВнесите меньшую сумму.\n";
+            }
         }
         else {
-            std::cout << "Недостаточно средств." << std::endl;
+            std::cout << "Недостаточно средств.\n";
         }
-        break;
     }
-    case 2: {
-        std::cout << "Выбраны чипсы." << std::endl;
-        std::cout << "Стоимость: " << dispenser.getCost() << std::endl;
-        std::cout << "Введите сумму: ";
-        int amount;
-        std::cin >> amount;
-
-        if (amount >= dispenser.getCost()) {
-            dispenser.makeSale();
-            cashReg.acceptAmount(dispenser.getCost());
-            std::cout << "Продукт продан. Сдача: " << amount - dispenser.getCost() << std::endl;
-        }
-        else {
-            std::cout << "Недостаточно средств." << std::endl;
-        }
-        break;
-    }
-    case 3: {
-        std::cout << "Выбрана жевательная резинка." << std::endl;
-        std::cout << "Стоимость: " << dispenser.getCost() << std::endl;
-        std::cout << "Введите сумму: ";
-        int amount;
-        std::cin >> amount;
-
-        if (amount >= dispenser.getCost()) {
-            dispenser.makeSale();
-            cashReg.acceptAmount(dispenser.getCost());
-            std::cout << "Продукт продан. Сдача: " << amount - dispenser.getCost() << std::endl;
-        }
-        else {
-            std::cout << "Недостаточно средств." << std::endl;
-        }
-        break;
-    }
-    case 4: {
-        std::cout << "Выбрано печенье." << std::endl;
-        std::cout << "Стоимость: " << dispenser.getCost() << std::endl;
-        std::cout << "Введите сумму: ";
-        int amount;
-        std::cin >> amount;
-
-        if (amount >= dispenser.getCost()) {
-            dispenser.makeSale();
-            cashReg.acceptAmount(dispenser.getCost());
-            std::cout << "Продукт продан. Сдача: " << amount - dispenser.getCost() << std::endl;
-        }
-        else {
-            std::cout << "Недостаточно средств." << std::endl;
-        }
-        break;
-    }
-    default:
-        std::cout << "Неверный выбор." << std::endl;
-        break;
+    else {
+        std::cout << "Товара нет в наличии:(\n";
     }
 }
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    dispenserType dispenser;
-    cashRegister cashReg;
+    setlocale(LC_ALL, "RU");
+    cashRegister cash(1000);
+    dispenserType sweets(150, 45), chips(84, 90), gum(50, 52), cookies(80, 60);
+
+    int choice;
     showSelection();
-    sellProduct(dispenser, cashReg);
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1:
+        sellProduct(sweets, cash);
+        break;
+    case 2:
+        sellProduct(chips, cash);
+        break;
+    case 3:
+        sellProduct(gum, cash);
+        break;
+    case 4:
+        sellProduct(cookies, cash);
+        break;
+    default:
+        std::cout << "Неправильный выбор.\n";
+        break;
+    }
+
     return 0;
 }
